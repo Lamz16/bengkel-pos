0c47289a7052283ee286cf26c64bde5231033b13
Andi Salam Syahputra <97329955+Lamz16@users.noreply.github.com>
2026-09-21 00:00:03 +0700

    feat(storage): add edit and delete location routes

--- src/server/routes/storageLocationRoutes.ts
+++ src/server/routes/storageLocationRoutes.ts
@@ -1,6 +1,13 @@
 import { Router } from 'express';
 import { storageLocationController } from '../controllers/storageLocationController';
+
 export const storageLocationRouter = Router();
+
 storageLocationRouter.get('/', storageLocationController.list);
 storageLocationRouter.post('/rack-layout', storageLocationController.createRackLayout);
 storageLocationRouter.post('/non-rack', storageLocationController.createNonRack);
+
+storageLocationRouter.put('/racks/:rackId', storageLocationController.updateRack);
+storageLocationRouter.delete('/racks/:rackId', storageLocationController.deleteRack);
+storageLocationRouter.put('/:id', storageLocationController.updateNonRack);
+storageLocationRouter.delete('/:id', storageLocationController.deleteNonRack);
