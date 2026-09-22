8a15e898b4d4f2c3b894d70861545b083491ca89
Andi Salam Syahputra <97329955+Lamz16@users.noreply.github.com>
2026-09-22 10:48:12 +0700

    feat(service): edit active transactions safely

--- src/server/routes/serviceRoutes.ts
+++ src/server/routes/serviceRoutes.ts
@@ -5,6 +5,7 @@ export const serviceRouter = Router();
 
 serviceRouter.get('/', (req, res) => serviceController.getServices(req, res));
 serviceRouter.post('/', (req, res) => serviceController.createService(req, res));
+serviceRouter.put('/:id', (req, res) => serviceController.updateService(req, res));
 serviceRouter.put('/:id/status', (req, res) => serviceController.updateStatus(req, res));
 serviceRouter.put('/:id/payment', (req, res) => serviceController.markPaid(req, res));
 serviceRouter.post('/:id/returns', (req, res) => serviceController.processReturn(req, res));
