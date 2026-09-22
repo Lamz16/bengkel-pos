83c79038ce6dfa9c08d951a7af5b24a609fa41ed
Andi Salam Syahputra <97329955+Lamz16@users.noreply.github.com>
2026-09-22 08:34:48 +0700

    feat(expenses): make expense categories dynamic

--- src/server/routes/financeRoutes.ts
+++ src/server/routes/financeRoutes.ts
@@ -2,6 +2,10 @@ import { Router } from 'express';
 import { financeController } from '../controllers/financeController';
 
 export const expenseRouter = Router();
+expenseRouter.get('/categories', (req, res) => financeController.getExpenseCategories(req, res));
+expenseRouter.post('/categories', (req, res) => financeController.createExpenseCategory(req, res));
+expenseRouter.put('/categories/:id', (req, res) => financeController.updateExpenseCategory(req, res));
+expenseRouter.delete('/categories/:id', (req, res) => financeController.deleteExpenseCategory(req, res));
 expenseRouter.get('/', (req, res) => financeController.getExpenses(req, res));
 expenseRouter.post('/', (req, res) => financeController.createExpense(req, res));
 expenseRouter.delete('/:id', (req, res) => financeController.deleteExpense(req, res));
